e56277068f59f25d9972a34d66d257cb2dc5b254
David C. Bishop <david@davidbishop.org>
2011-06-21 15:10:15 +1000

    Fixed indentation.

--- ass1.c
+++ ass1.c
@@ -110,9 +110,9 @@ int main(int argc, char* argv[]) {
 		}
 		
 		if((pid = fork() == 0)) {
-		    char data[MAXDATASIZE];
-		    char message[MAXDATASIZE];
-    	    char* filename = NULL;
+			char data[MAXDATASIZE];
+			char message[MAXDATASIZE];
+			char* filename = NULL;
 
 			char address_str[INET_ADDRSTRLEN];
 			inet_ntop(AF_INET, &(their_addr.sin_addr), address_str, INET_ADDRSTRLEN);
@@ -120,98 +120,98 @@ int main(int argc, char* argv[]) {
 			snprintf(message, MAXDATASIZE-1,"New connection from %s.", address_str);
 			logmsg(message);
 		
-		    char buf[MAXDATASIZE];
-		    int numbytes;
-		    if ((numbytes=recv(fd_new, buf, MAXDATASIZE-1, 0)) == -1) {
-			    perror("recv");
-			    close(fd_new);
-			    exit(1);
-		    }
-		    printf("Recieved %d bytes of data.\n", numbytes);
+			char buf[MAXDATASIZE];
+			int numbytes;
+			if ((numbytes=recv(fd_new, buf, MAXDATASIZE-1, 0)) == -1) {
+				perror("recv");
+				close(fd_new);
+				exit(1);
+			}
+			printf("Recieved %d bytes of data.\n", numbytes);
 		
-		    char *line = strtok(buf, "\n");
-		    while(line != NULL) {
-			    char *tok = strtok(buf, " ");
-			    int state = STATE_NONE;
-			    int stateIgnoreCount = 0;
-			    while (tok != NULL) {
-				    switch(state) {
-					    /* Looks for a valid HTML request */
-					    case STATE_NONE:
-						    if(strncmp("GET", tok, 3) == 0) {
-							    printf("GET REQUEST\n");
-							    state = STATE_GET;
-						    } else {
-							    printf("Skipping unknown token '%s'\n", tok);
-						    }
-						    break;
-					    /* processes a get request */
-					    case STATE_GET:
-						    printf("Filename: %s\n", tok);
-						    state = STATE_NONE;
+			char *line = strtok(buf, "\n");
+			while(line != NULL) {
+				char *tok = strtok(buf, " ");
+				int state = STATE_NONE;
+				int stateIgnoreCount = 0;
+				while (tok != NULL) {
+					switch(state) {
+						/* Looks for a valid HTML request */
+						case STATE_NONE:
+							if(strncmp("GET", tok, 3) == 0) {
+								printf("GET REQUEST\n");
+								state = STATE_GET;
+							} else {
+								printf("Skipping unknown token '%s'\n", tok);
+							}
+							break;
+						/* processes a get request */
+						case STATE_GET:
+							printf("Filename: %s\n", tok);
+							state = STATE_NONE;
 						
-						    if(strcmp(tok, "/") == 0) {
-							    filename = "index.html";
-						    } else if (tok[0] == '/') { /* nuke leading '/' */
-							    printf("Nuke leading /\n");
-							    filename = tok+1;
-						    } else {
-							    filename = tok;
-						    }
+							if(strcmp(tok, "/") == 0) {
+								filename = "index.html";
+							} else if (tok[0] == '/') { /* nuke leading '/' */
+								printf("Nuke leading /\n");
+								filename = tok+1;
+							} else {
+								filename = tok;
+							}
 						
-						    break;
-					    /* Ignores trailng data */
-					    case STATE_IGNORE:
-						    stateIgnoreCount--;
-						    if(stateIgnoreCount < 1) {
-							    state = STATE_NONE;
-						    }
-						    printf("Skipping token '%s'...\n", tok);
-						    break;
-				    }
+							break;
+						/* Ignores trailng data */
+						case STATE_IGNORE:
+							stateIgnoreCount--;
+							if(stateIgnoreCount < 1) {
+								state = STATE_NONE;
+							}
+							printf("Skipping token '%s'...\n", tok);
+							break;
+					}
 				
-				    tok = strtok(NULL," ");
-			    }
-			    line = strtok(NULL, "\n");
-		    }
-		    
-		    if(filename != NULL) {
-			    int fd;
-			    int flags = O_RDONLY;
-			    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+					tok = strtok(NULL," ");
+				}
+				line = strtok(NULL, "\n");
+			}
 			
-			    char realfile[MAXDATASIZE];
-			    snprintf(realfile, MAXDATASIZE, "%s%s%s", documentRoot, SEPERATOR, filename);
-			    printf("Opening file '%s'...\n", realfile);
-			    fd = open(realfile, flags, mode);
+			if(filename != NULL) {
+				int fd;
+				int flags = O_RDONLY;
+				mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
 			
-			    /* Display a html 404 page if it exists */
-			    if(fd == -1) {
-				    snprintf(realfile, MAXDATASIZE, "%s%s%s", documentRoot, SEPERATOR, "404.html"); 
-				    printf("Loading 404 page: '%s'\n", realfile);
-				    fd = open(realfile, flags, mode);
-			    }
+				char realfile[MAXDATASIZE];
+				snprintf(realfile, MAXDATASIZE, "%s%s%s", documentRoot, SEPERATOR, filename);
+				printf("Opening file '%s'...\n", realfile);
+				fd = open(realfile, flags, mode);
 			
-			    /* Display a real basic plain text 404 message if no html */
-			    if (fd == -1) {
-				    perror("file");
-				    snprintf(data, MAXDATASIZE-1,"404!\n");
-				    if (send(fd_new, data, strlen(data), 0) == -1) {
-					    perror("send");
-					    close(fd_new);
-					    exit(1);
-				    }
+				/* Display a html 404 page if it exists */
+				if(fd == -1) {
+					snprintf(realfile, MAXDATASIZE, "%s%s%s", documentRoot, SEPERATOR, "404.html"); 
+					printf("Loading 404 page: '%s'\n", realfile);
+					fd = open(realfile, flags, mode);
+				}
+			
+				/* Display a real basic plain text 404 message if no html */
+				if (fd == -1) {
+					perror("file");
+					snprintf(data, MAXDATASIZE-1,"404!\n");
+					if (send(fd_new, data, strlen(data), 0) == -1) {
+						perror("send");
+						close(fd_new);
+						exit(1);
+					}
 				}
 
-			    int nbytes = 100;
-			    while( (nbytes = read(fd, data, MAXDATASIZE)) > 0) {
-				    write(fd_new, data, nbytes);
-			    }
-			    printf("Closing file...\n");
-			    close(fd);
-		    }
+				int nbytes = 100;
+				while( (nbytes = read(fd, data, MAXDATASIZE)) > 0) {
+					write(fd_new, data, nbytes);
+				}
+				printf("Closing file...\n");
+				close(fd);
+			}
 		}
-    	close(fd_new);
+		close(fd_new);
 	}
 	
 	exit(0);
